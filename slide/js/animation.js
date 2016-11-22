c67f847d9590c8b87990afa9c28ecef3f7c0eb8e
Phillip Salazar <areabio52@gmail.com>
2016-11-22 09:14:08 -0600

    new project added
    a slide show.

---
+++ slide/js/animation.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+// Title animations
+$('#titlez').animate({ color: 'black'}, 1000);
+$('#titlez').animate({ color: 'white'},3000);
+
+});
---
+++ slide/js/app.js
@@ -0,0 +1,10 @@
+
+
+Reveal.initialize();
+
+
+  $(document).ready(function(){
+
+
+
+});
