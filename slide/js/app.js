5b9a19b258268b0a8313446043f74ec6eb1550e0
Phillip Salazar <areabio52@gmail.com>
2016-11-29 15:25:34 -0600

    slides update

--- slide/js/app.js
+++ slide/js/app.js
@@ -9,8 +9,15 @@ $(document).ready(function(){
 setInterval(function() {
   $(".cat").toggleClass("blink").delay(500);
   $(".cat").toggleClass("lolz");
+
+//  $("#topleft").toggleClass(".green").delay(500);
+//  $("#topleft").toggleClass(".lolz");
+//  $(".redp").toggleClass(".lol").delay(500);
+//  $(".redp").toggleClass(".lol");
+
   console.log("toggled!");
 }, 500);
+
 
 
 });
